91afcfbf4a020712cfebd74b6d5b855eb4727e63
Christine Ross <christinejoyross@gmail.com>
2021-01-22 16:54:49 -0800

    create posts client side

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { useQuery } from '@apollo/react-hooks'
 import gql from 'graphql-tag';
 import { Grid } from 'semantic-ui-react'
+import { AuthContext } from '../context/auth'
 import PostCard from '../components/PostCard'
+import PostForm from '../components/PostForm';
+import { FETCH_POSTS_QUERY } from '../util/graphql'
 
 const Home = () => {
+    const { user } = useContext(AuthContext)
 
     const { loading, data: { getPosts: posts } = {} } = useQuery(FETCH_POSTS_QUERY)
 
@@ -16,6 +20,11 @@ const Home = () => {
                 <h1>Recent Posts</h1>
             </Grid.Row>
             <Grid.Row>
+                {user && (
+                    <Grid.Column>
+                        <PostForm />
+                    </Grid.Column>
+                )}
                 {loading ? (
                     <h1>Loading posts...</h1>
                 ) : (
@@ -31,26 +40,6 @@ const Home = () => {
     )
 }
 
-const FETCH_POSTS_QUERY = gql`
-    {
-        getPosts{
-            id 
-            body 
-            createdAt 
-            username 
-            likeCount
-            likes {
-            username
-        }
-        commentCount
-        comments{
-            id 
-            username 
-            createdAt 
-            body
-        }
-    }
-    }
-`
+
 
 export default Home
